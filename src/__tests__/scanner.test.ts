22ded642e88469336f47fc0da77046030e16c63c
Dudu Popkhadze <dudupopkhadze@gmail.com>
2022-08-04 00:28:30 +0400

    fix test

--- src/__tests__/scanner.test.ts
+++ src/__tests__/scanner.test.ts
@@ -1,15 +1,15 @@
 import { Scanner } from "../Compiler/Scanner";
 
 describe("Scanner Test", () => {
-  test("test #1", () => {
+  test("#1", () => {
     const source = "turnRight();";
     const onError = jest.fn();
     const scanner = new Scanner(source, onError);
     const tokens = scanner.scanTokens();
-    expect(1).toBe(1);
+    expect(tokens.length).toBe(5);
   });
 
-  test("test #2", () => {
+  test("#2", () => {
     const source = "for(let i = 0; i < 5; i = i + 1) {}";
     const onError = jest.fn();
     const scanner = new Scanner(source, onError);
