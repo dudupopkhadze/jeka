614a1a30012910fd0503d35759d723a08a4fa0a8
Dudu Popkhadze <dudupopkhadze@gmail.com>
2023-03-24 22:49:49 +0400

    update CIRCLE_RADIUS

--- src/config/board.ts
+++ src/config/board.ts
@@ -1,6 +1,6 @@
 import { BoardConfig, BoardSizeLabel } from "../types";
 
-export const CIRCLE_RADIUS = 5;
+export const CIRCLE_RADIUS = 3.5;
 export const CIRCLE_DIAMETER = 2 * CIRCLE_RADIUS;
 export const CANVAS_WIDTH = 600;
 export const CANVAS_HEIGHT = 600;
